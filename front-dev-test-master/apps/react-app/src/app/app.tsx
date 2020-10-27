cec3d2b3e5e6408c9c063198f272141420b824dc
Will Toozs <cris.hobson@hotmail.com>
2020-10-27 13:32:45 +0100

    list loading now

--- front-dev-test-master/apps/react-app/src/app/app.tsx
+++ front-dev-test-master/apps/react-app/src/app/app.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Dispatch, useEffect } from 'react';
 import { TextField } from '@material-ui/core';
 import { List } from '@material-ui/core';
 import { ListItem } from '@material-ui/core';
@@ -6,19 +6,11 @@ import { ListItemText } from '@material-ui/core';
 import { Provider, useDispatch, useSelector } from 'react-redux'
 
 import './app.scss';
-import { fetchMovieList } from './slices/movieListSlice';
+import { fetchMovieSearchList } from './slices/movieListSlice';
 import rootReducer, { RootState } from './rootReducer';
 import { createStore, applyMiddleware } from '@reduxjs/toolkit';
 import store from './store';
-
-class Omdb {
-  // Function to recover movies from API
-  getMovies(search: string) {
-    // use a dispatch on a reducer from the movieListSlice
-    const dispatch = useDispatch()
-    dispatch(fetchMovieList(search))
-  }
-}
+import { MovieSearchResult } from 'custom-project-typings';
 
 // I had to dig hard to find that this was supposed to be how the project is set up... 
 // https://github.com/reduxjs/rtk-github-issues-example/blob/master/src/index.tsx
@@ -32,23 +24,39 @@ export const AppWrapper = () => {
   )
 }
 
-const App: React.FC = () => {
-  let omdbObject: Omdb = new Omdb
-  // useSelector to use data from store and subscribe to updates
-  const movieList = useSelector(
-    (state: RootState) => state.movieList
-  )
-  omdbObject.getMovies("test")
+class Omdb {
+  /* 
+  ** Function to recover movies from API
+  ** Takes dispatch as parameter to avoid 'not called in function component' issues 
+  ** I'm tryin my best here ¯\_(ツ)_/¯
+  */
+  searchMovies(search: string, dispatch: Dispatch<any>) {
+    // use a dispatch on a reducer from the movieListSlice
+    dispatch(fetchMovieSearchList(search))
+  }
+}
 
+const App: React.FC = () => {
+  const omdbObject: Omdb = new Omdb
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    omdbObject.searchMovies("test", dispatch)    
+  })
+  // useSelector to use data from store and subscribe to updates
+  const movieList: MovieSearchResult = useSelector(
+    (state: RootState) => state.movieList.movieList
+  )
+  
   return <div className="app">
     <TextField label="Search" margin="normal" variant="outlined"/>
-    {/* <List >
-      {omdbObject.movies.map(({imdbId, posterUrl, title, ...rest }) => (
+    <List >
+      {movieList.map(({imdbId, posterUrl, title, ...rest }) => (
         <ListItem key={imdbId} button {...rest}>
           <ListItemText>{title}</ListItemText>
         </ListItem>
       ))}
-    </List> */}
+    </List>
   </div>;
 };
 
--- front-dev-test-master/apps/react-app/src/app/slices/movieListSlice.tsx
+++ front-dev-test-master/apps/react-app/src/app/slices/movieListSlice.tsx
@@ -1,6 +1,7 @@
 import { createSlice, createAsyncThunk, PayloadAction } from '@reduxjs/toolkit';
 import { Movie, MovieSearchResult } from 'custom-project-typings';
 import { AppThunk } from '../store';
+import durableJsonLint from 'durable-json-lint'
 
 const API_URL = 'http://www.omdbapi.com/?';
 const API_KEY = '&apikey=23aaa32';
@@ -19,12 +20,12 @@ const movieListSlice = createSlice({
   name: 'movieList',
   initialState: initialMovieListState,
   reducers: {
-    getMovieListSuccess(state, action: PayloadAction<MovieSearchResult>) {
+    getMovieSearchListSuccess(state, action: PayloadAction<MovieSearchResult>) {
       // Need to convert data to Movie format
       state.movieList = action.payload;
       state.loading = 'success';
     },
-    getMovieListFailed(state, action: PayloadAction<string>) {
+    getMovieSearchListFailed(state, action: PayloadAction<string>) {
       state.movieList = [];
       state.loading = 'failed';
     },
@@ -32,31 +33,58 @@ const movieListSlice = createSlice({
 });
 
 export const {
-  getMovieListSuccess,
-  getMovieListFailed,
+  getMovieSearchListSuccess,
+  getMovieSearchListFailed,
 } = movieListSlice.actions;
+
+function formatYear(year) {
+  var from, to;
+  year = year.split('–');
+
+  if (year.length === 2) {
+      from = +year[0];
+
+      if (year[1]) {
+          to = +year[1];
+      }
+      return { from: from, to: to };
+  }
+  return +year;
+}
 
 /*
 ** more on async thunks here https://redux-toolkit.js.org/usage/usage-with-typescript#createasyncthunk
 ** Here I copied the 'AppThunk' type as it applies to our purposes and is common: 
 ** https://redux-toolkit.js.org/tutorials/advanced-tutorial#logic-for-fetching-github-repo-details
 */
-export const fetchMovieList = (search: string): AppThunk => async (
+export const fetchMovieSearchList = (search: string): AppThunk => async (
   dispatch // ,getState -> to access state data in the thunk before executing an action function (see more above)  
 ) => {
-  const searchParam = '&s' + search;
+  const searchParam = "&s=" + search;
   // Declare data here so it's usable by Success dispatch...
-  let data;
+  let jsonData
   try {
     const response = await fetch(API_URL + API_KEY + searchParam);
-    data = await response.json();
+    // .json() not working, using work around, like here at l.217 https://github.com/misterhat/omdb/blob/master/index.js
+    const data = await response.text();
+    jsonData = JSON.parse(data)
+    console.log(jsonData)
   } catch (err) {
-    dispatch(getMovieListFailed(err.toString()));
+    dispatch(getMovieSearchListFailed(err.toString()));
     return;
   }
+  
+  let movieSearchResults = jsonData.Search.map(function (movie) {
+    return {
+        title: movie.Title,
+        year: formatYear(movie.Year),
+        imdbId: movie.imdbID,
+        type: movie.Type,
+        posterUrl: movie.Poster
+    };
+  })
   // ... Success dispatch placed here so error's do not result in uncalled-for catch
-  console.log('Success, data: ' + data);
-  dispatch(getMovieListSuccess(data));
+  dispatch(getMovieSearchListSuccess(movieSearchResults));
 };
 
 export default movieListSlice.reducer;
